cdcd65e14d26010a2df61d5d5c1554b4a0e614a2
ruabidd <Daniil.BORODIN@raiffeisen.ru>
2025-08-04 14:01:06 +0300

    [NF] CI tests

--- tests/ui/e2e-with-fixture.spec.ts
+++ tests/ui/e2e-with-fixture.spec.ts
@@ -36,7 +36,7 @@ test('search for an existing order created through API with fixture', async ({
   await expect(page.getByText('OPEN')).toBeVisible()
 })
 
-test.only('search for order with delivered status using mock fixture', async ({ mainPage }) => {
+test('search for order with delivered status using mock fixture', async ({ mainPage }) => {
   await mainPage.getByTestId('openStatusPopup-button').click()
   await mainPage.getByTestId('searchOrder-input').fill('9999')
   await mainPage.getByTestId('searchOrder-submitButton').click()
